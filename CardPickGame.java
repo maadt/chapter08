bd9783704d513d712f732f6155f2b79321f3f02d
maadt <147260993+maadt@users.noreply.github.com>
2024-02-15 16:53:35 +0900

    kadai3修正

--- CardPickGame.java
+++ CardPickGame.java
@@ -1,32 +1,58 @@
+import java.util.List;
+import java.util.Arrays;
+import java.util.ArrayList;
+
 class CardPickGame {
     private int maxBetCoin = 100;//最大ベット枚数
-    private int deskSetCount = 2;//カードセット数
+    private int deckSetCount = 2;//カードセット数
     private int possessionCoin;//所持コイン数
-    public int A;//一枚目のカード
-    public int B;//二枚目のカード
-    public int C;//AとBの合計値
 
     public CardPickGame(int possessionCoin) {
         this.possessionCoin = possessionCoin;
     }
 
     public void execute() {
-        this.getCard();
+        getCard();
     }
 
     private int getCard() {
-        A = GameUtils.getRandomInt(10) + 1;
-        B = GameUtils.getRandomInt(10) + 1;
-        C = 0;
-        if (A == B) {
-            getCard();
-        } else {
-            C = A + B;
-            System.out.print("Cards drawn are" + " " + A + " ");
-            System.out.print("and" + " " + B + ", total is" + " " + C);
-            System.out.println();
+        List<List<Integer>> setDeck = new ArrayList<List<Integer>>();
+        // List<List<Integer>>：Integer型のList型オブジェクトを格納する
+        List<Integer> onePair = new ArrayList<Integer>(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10));
+        // Arrays.asList()：与えられた要素を持つ固定サイズのリストを返す
+
+        for (int i = 0; i < this.deckSetCount; i++) {
+            setDeck.add(i, onePair);
+            // setDeck：配列
+            // add()：要素の追加
+            // i, onePair：配列のキー, 追加する要素
         }
-        return C;
+
+        int randNumA1;
+        int randNumA2;
+        int randNumB1;
+        int randNumB2;
+        while (true) { // 無限ループ
+            randNumA1 = GameUtils.getRandomInt(2);
+            randNumA2 = GameUtils.getRandomInt(10);
+            randNumB1 = GameUtils.getRandomInt(2);
+            randNumB2 = GameUtils.getRandomInt(10);
+            if (!(randNumA1 == randNumB1 && randNumA2 == randNumB2)) {
+                break; // 無限ループを停止させる
+            }
+            // while文内のif文の結果自体、ループ全体の制御フローに影響しない
+        }
+
+        int cardA = setDeck.get(randNumA1).get(randNumA2);
+        // setDeck：リスト
+        // randNumA1：インデックスのキー番号
+        // randNumA2：サブリストのキー番号
+        int cardB = setDeck.get(randNumB1).get(randNumB2);
+        int userNumSum = cardA + cardB;
+
+        System.out.println("Cards drawn are " + cardA + " and " + cardB + ", total is " + userNumSum + ".");
+
+        return userNumSum;
     }
 
 }
